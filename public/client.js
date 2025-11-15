657a46d17091f359bc573ce9203f4c6a099fbe3b
dr-ianX <4dr.ian.art@gmail.com>
2025-11-15 04:23:44 -0700

    .

--- public/client.js
+++ public/client.js
@@ -379,7 +379,7 @@ class GridyClient {
         if (this.posts.length === 0) {
             gridContainer.innerHTML = `
                 <div class="loading">
-                    <h3>¡Bienvenido al DJMesh! 🌟</h3>
+                    <h3>¡Bienvenido al DJMesh de Los Cabos! 🌟</h3>
                     <p>Sé el primero en publicar haciendo doble click en cualquier lugar</p>
                     <p>O usa el botón naranja en la esquina inferior derecha</p>
                 </div>
